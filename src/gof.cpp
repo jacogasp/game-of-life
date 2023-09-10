c4874f532af9034a10f5659a2e903e733fa89f17
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2023-09-10 16:56:41 +0200

    Add FixNamespaceComments to clang-format

--- src/gof.cpp
+++ src/gof.cpp
@@ -1,1 +1,22 @@
 #include "gof.hpp"
+
+namespace gof {
+GameOfLife::GameOfLife(unsigned int width, unsigned int height)
+    : m_window { sf::VideoMode { width, height }, "Game Of Life" }
+{
+}
+
+void GameOfLife::run()
+{
+    sf::Event event;
+    while (m_window.isOpen()) {
+        while (m_window.pollEvent(event)) {
+            if (event.type == sf::Event::Closed) {
+                m_window.close();
+            }
+        }
+        m_window.clear(sf::Color::Black);
+        m_window.display();
+    }
+}
+} // namespace gof

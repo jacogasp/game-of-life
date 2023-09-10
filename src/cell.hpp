84c8909585a10e77fa99c845b28b7cefacfd2fe6
Jacopo Gasparetto <jacopo.gasparetto@gmail.com>
2023-09-10 17:51:21 +0200

    Add Cell class

---
+++ src/cell.hpp
@@ -0,0 +1,19 @@
+#ifndef CELL_HPP
+#define CELL_HPP
+
+#include <SFML/Graphics.hpp>
+
+namespace gof {
+class Cell : public sf::Drawable {
+    sf::RectangleShape m_shape;
+    bool m_is_active;
+
+public:
+    Cell(float size = 5);
+    void set_position(sf::Vector2f const& position);
+    [[nodiscard]] bool is_active() const;
+    void set_is_active(bool is_active);
+    void draw(sf::RenderTarget& target, sf::RenderStates states) const override;
+};
+} // namespace gof
+#endif // CELL_HPP
